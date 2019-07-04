3cb87f591012ce33702124c52649d97fde17084c
Fabian Holler <fabian.holler@simplesurance.de>
2019-07-04 11:39:30 +0200

    jsonlogs2txt: use loglevel string field instead of removed lvl int field

--- nomad-logs/jsonlogs2txt.py
+++ nomad-logs/jsonlogs2txt.py
@@ -3,24 +3,6 @@
 import fileinput
 import json
 
-def log_lvl_to_str(lvl):
-    if not isinstance(lvl, int) and not (isinstance(lvl, str) and lvl.isdigit()):
-        return "unknown"
-
-    ilvl = int(lvl)
-
-    if ilvl == 0:
-        return "crit"
-    elif ilvl == 1:
-        return "err"
-    elif ilvl == 2:
-        return "warn"
-    elif ilvl == 3:
-        return "info"
-    elif ilvl == 4:
-        return "debug"
-    return "unknown"
-
 for line in fileinput.input():
     try:
         js = json.loads(line)
@@ -30,7 +12,7 @@ for line in fileinput.input():
 
     ts = js.get("time_iso8601", "timestamp-unknown")
     app = js.get("app", "app-unknown")
-    lvl = log_lvl_to_str(js.get("lvl", ""))
+    lvl = js.get("loglevel", "logl-lvl-unset")
     msg = js.get("msg", "").strip()
 
     keys = ("time_iso8601", "app", "lvl", "msg", "ver")
